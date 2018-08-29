2f7e5a165f5d67aba651e11c3b746f497bff583d
David A York <medmatix@gmail.com>
2018-08-29 16:19:28 -0400

    reloaded project
    version 5m3

---
+++ CopyBookLists.bat
@@ -0,0 +1,50 @@
+Dir A* /s  > LibList-A.txt
+Dir B * /s  > LibList-B.txt
+Dir BF* /s  > LibList-BF.txt
+Dir BL* /s  > LibList-BL.txt
+Dir BM* /s  > LibList-BM.txt
+Dir BP* /s  > LibList-BP.txt
+Dir BQ* /s  > LibList-BQ.txt
+Dir BR* /s  > LibList-BQ.txt
+Dir BS* /s  > LibList-BQ.txt
+Dir BT* /s  > LibList-BQ.txt
+Dir D* /s  > LibList-D.txt
+Dir G* /s  > LibList-G.txt
+Dir GV* /s  > LibList-GV.txt
+Dir H * /s  > LibList-H.txt
+Dir HB* /s  > LibList-HB.txt
+Dir HD* /s  > LibList-HD.txt
+Dir HE* /s  > LibList-HE.txt
+Dir HF* /s  > LibList-HF.txt
+Dir HM* /s  > LibList-HM.txt
+Dir HV* /s  > LibList-HV.txt
+Dir J* /s  > LibList-J.txt
+Dir K* /s  > LibList-K.txt
+Dir L* /s  > LibList-L.txt
+Dir M* /s  > LibList-M.txt
+Dir N* /s  > LibList-N.txt
+Dir P * /s  > LibList-P.txt
+Dir PA* /s  > LibList-PA.txt
+Dir PN* /s  > LibList-PN.txt
+Dir Q *   > LibList-Q.txt
+Dir QA-*   > LibList-QA.txt
+Dir QA1* /s  > LibList-QA1.txt
+Dir QA7* /s  > LibList-QA7.txt
+Dir QB* /s  > LibList-QB.txt
+Dir QC* /s  > LibList-QC.txt
+Dir QD* /s  > LibList-QD.txt
+Dir QE* /s  > LibList-QE.txt
+Dir QH* /s  > LibList-QH.txt
+Dir R* /s  > LibList-R.txt
+Dir S* /s  > LibList-S.txt
+Dir T* /s  > LibList-T.txt
+Dir TA* /s  > LibList-TA.txt
+Dir TK* /s  > LibList-TK.txt
+Dir TR* /s  > LibList-TR.txt
+Dir TT* /s  > LibList-TT.txt
+Dir TX* /s  > LibList-TX.txt
+Dir U* /s  > LibList-U.txt
+Dir V* /s  > LibList-V.txt
+Dir Z* /s  > LibList-Z.txt
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# crunches-data
-New Website Versions updated and cleaned files
+# crunches-data 
+
+## Medmatix Professional Website: Maintenance code
+
+This is the repository for development and maintenance of my professional website. It is also a test-bed for my web page programming experiments in html/css/javascript as well as various frameworks I use or am trying out
+  
+https://medmatix.github.io/crunches-data/ 
+  
+The Production Site is currently at https://crunches-data.appspot.com
+
